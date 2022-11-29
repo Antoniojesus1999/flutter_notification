362210399af93f0e5d32a1a367af2aa9caf9e500
Antonio Jesús Ponce Vela <antoniojesuspv99@gmail.com>
2022-11-29 22:42:23 +0100

    envio de parametros en las notificaciones

--- lib/services/push_notification_service.dart
+++ lib/services/push_notification_service.dart
@@ -1,20 +1,29 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushNotificationService{
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
+  static final StreamController<String> _messageStream = StreamController.broadcast();
+  static Stream<String> get messagesStream => _messageStream.stream;
 
 // Dependiendo donde se este ejecutando la aplicación entra en un método otro
+//message.notification.title message.notification.body
   static Future _backgroudHandler(RemoteMessage message) async {
-    print('entra por el background Handler ${message.messageId}');
+    //print('entra por el background Handler ${message.messageId}');
+    _messageStream.add(message.notification?.title ?? 'No title');
   }
 
   static Future _onMessageHandler(RemoteMessage message) async{
-    print(' entra por el segundo onMessage Hnadler ${message.messageId}');
+    //print(' entra por el segundo onMessage Hnadler ${message.messageId}');
+    print(message.data);
+    _messageStream.add(message.notification?.title ?? 'No title');
   }
   static Future _onMessageOpenApp(RemoteMessage message) async {
-    print('onMessageOpenApp Handler ${message.messageId}');
+    //print('onMessageOpenApp Handler ${message.messageId}');
+    _messageStream.add(message.notification?.title ?? 'No title');
   }
 
   static Future initializeApp() async {
@@ -25,5 +34,8 @@ class PushNotificationService{
     FirebaseMessaging.onBackgroundMessage(_backgroudHandler);
     FirebaseMessaging.onMessage.listen(_onMessageHandler);
     FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenApp);
+  }
+  static closeStreams(){
+    _messageStream.close();
   }
 }
